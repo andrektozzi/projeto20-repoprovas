ec362562760ec378ebbe0ceceaf1dbd89b231fa0
andrektozzi <andrektozzi@gmail.com>
2022-09-19 14:50:12 -0300

    test: add tests for get tests by discipline

--- tests/tests.test.ts
+++ tests/tests.test.ts
@@ -9,7 +9,7 @@ beforeEach(async () => {
     await client.$executeRaw`TRUNCATE TABLE "tests" RESTART IDENTITY CASCADE`;
 });
 
-describe("Test POST /tests", () => {
+describe("TEST POST /tests", () => {
     it("Deve retornar status 201 se a prova for criada corretamente", async () => {
         const user = await userSignUpFactory();
         await supertest(app).post(`/signup`).send(user);
@@ -118,6 +118,37 @@ describe("Test POST /tests", () => {
         expect(result.status).toBe(404);
      });
 });
+
+describe("TEST GET /tests/disciplines", () => {
+     it("Deve retornar status 200 se conseguir visualizar os testes corretamente", async () => {
+        const user = await userSignUpFactory();
+        await supertest(app).post(`/signup`).send(user);
+    
+        const userLogin = { email: user.email, password: user.password };
+        const response = await supertest(app).post(`/signin`).send(userLogin);
+        const { token } = response.body;
+    
+        const result = await supertest(app).get(`/tests/disciplines`).auth(token, { type: "bearer" });
+    
+        expect(result.status).toBe(200);
+        expect(result.body).toBeInstanceOf(Object);
+    });
+
+    it("Deve retornar status 500 se o token for inválido", async () => {
+        const user = await userSignUpFactory();
+        await supertest(app).post(`/signup`).send(user);
+
+        const userLogin = { email: user.email, password: user.password };
+        const response = await supertest(app).post(`/signin`).send(userLogin);
+        let { token } = response.body;
+        token = token + "a";
+
+        const test = await testFactory();
+        const result = await supertest(app).get(`/tests/disciplines`).send(test).auth(token, { type: "bearer" });
+
+        expect(result.status).toBe(500);
+    });
+})
 
 afterAll(async () => {
     await client.$disconnect();
