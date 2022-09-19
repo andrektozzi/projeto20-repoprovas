2f50987a7198fb8ba0de3bb87f071ec893be06f6
andrektozzi <andrektozzi@gmail.com>
2022-09-19 14:53:32 -0300

    test: add tests for get tests by teachers

--- tests/tests.test.ts
+++ tests/tests.test.ts
@@ -148,7 +148,38 @@ describe("TEST GET /tests/disciplines", () => {
 
         expect(result.status).toBe(500);
     });
-})
+});
+
+describe("TEST GET /tests/teachers", () => {
+    it("Deve retornar status 200 se conseguir visualizar os testes corretamente", async () => {
+        const user = await userSignUpFactory();
+        await supertest(app).post(`/signup`).send(user);
+    
+        const userLogin = { email: user.email, password: user.password };
+        const response = await supertest(app).post(`/signin`).send(userLogin);
+        const { token } = response.body;
+    
+        const result = await supertest(app).get(`/tests/teachers`).auth(token, { type: "bearer" });
+  
+        expect(result.status).toBe(200);
+        expect(result.body).toBeInstanceOf(Object);
+    });
+
+   it("Deve retornar status 500 se o token for inválido", async () => {
+        const user = await userSignUpFactory();
+        await supertest(app).post(`/signup`).send(user);
+
+        const userLogin = { email: user.email, password: user.password };
+        const response = await supertest(app).post(`/signin`).send(userLogin);
+        let { token } = response.body;
+        token = token + "a";
+
+        const test = await testFactory();
+        const result = await supertest(app).get(`/tests/teachers`).send(test).auth(token, { type: "bearer" });
+
+        expect(result.status).toBe(500);
+    });
+});
 
 afterAll(async () => {
     await client.$disconnect();
